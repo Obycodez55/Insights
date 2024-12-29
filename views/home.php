accde4d40b9f4cfef02f537c695b3cf3ac66d14d
Obycodez55 <obikoyaadebayo55@gmail.com>
2024-12-29 15:16:21 +0100

    Add About section with personal details, education, and career journey; enhance styles and layout for better presentation

--- views/home.php
+++ views/home.php
@@ -13,57 +13,65 @@
         <div class="about">
             <div class="about-section">
                 <div class="info">
-                    <span class="icon">
-                        <i class="fa-solid fa-user"></i>
-                    </span>
-                    <h3>Early Life and Interests</h3>
+                    <div>
+                        <span class="icon">
+                            <i class="fa-solid fa-user"></i>
+                        </span>
+                        <h3>Early Life and Interests</h3>
+                    </div>
                     <p>I was born in Ibadan, Oyo State, but my roots trace back to Ogun State. Growing up, I never imagined a future in technology. My aspirations leaned toward the arts—becoming a lawyer, an actor, a musician, or even a public speaker. Those were the paths that felt natural to me. <br />
 
-                    Things took an unexpected turn during my senior secondary school when I chose to study science. It surprised not just my peers but even me. However, I soon realized that science was a better fit for my abilities and interests. Over time, this realization led me to pursue a degree in Computer Science, where I found a natural affinity for problem-solving and technology.</p>
+                        Things took an unexpected turn during my senior secondary school when I chose to study science. It surprised not just my peers but even me. However, I soon realized that science was a better fit for my abilities and interests. Over time, this realization led me to pursue a degree in Computer Science, where I found a natural affinity for problem-solving and technology.</p>
                 </div>
                 <div class="img">
-                    <img src="<?= base_url('/images/banner_1.png') ?>" alt="Profile Picture">
+                    <img src="<?= base_url('/images/early_1.jpg') ?>" alt="Profile Picture">
                 </div>
             </div>
             <div class="about-section">
                 <div class="img">
-                    <img src="<?= base_url('/images/banner_1.png') ?>" alt="Profile Picture">
+                    <img src="<?= base_url('/images/books_2.jpg') ?>" alt="Profile Picture">
                 </div>
 
                 <div class="info">
-                    <span class="icon">
+                   <div>
+                   <span class="icon">
                         <i class="fa-solid fa-graduation-cap"></i>
                     </span>
                     <h3>Education and Career</h3>
+                   </div>
                     <p>I am currently pursuing a degree in Computer Science, where I&apos;ve maintained a strong academic record as the top of my class. My coursework has provided me with a solid foundation in programming, algorithms, and web development. Through hands-on projects, I&apos;ve gained practical skills and the confidence to tackle real-world challenges.</p>
                 </div>
             </div>
             <div class="about-section">
                 <div class="info">
+                    <div>
                     <span class="icon">
                         <i class="fa-solid fa-code"></i>
                     </span>
                     <h3>Entry into Software Engineering</h3>
+                    </div>
                     <p>My journey into software engineering began in 2019, during the COVID-19 period. Encouraged by my brother to make the most of my time, I started learning HTML—and that decision sparked a passion for coding. Over the past five years, I&apos;ve expanded my skills and become proficient in <span>JavaScript</span>, <span>TypeScript</span>, <span>Python</span>, <span>Java</span>, and technologies like <span>React</span>, <span>Next.js</span>, <span>Node.js</span>, <span>NestJS</span>, <span>ExpressJS</span>, <span>jQuery</span>, <span>WebSockets</span>, <span>Firebase</span>, <span>Redux</span>, <span>Zustand</span>, and more. I am also currently exploring <span>PHP</span> and <span>Go</span> to broaden my expertise.</p>
 
                     To enhance my knowledge and stand out as a software engineer, I&apos;ve taken courses on data structures and algorithms, software architecture, and systems design, equipping me to build scalable and efficient applications.</p>
                 </div>
                 <div class="img">
-                    <img src="<?= base_url('/images/banner_1.png') ?>" alt="Profile Picture">
+                    <img src="<?= base_url('/images/code_1.jpg') ?>" alt="Profile Picture">
                 </div>
             </div>
             <div class="about-section">
                 <div class="img">
-                    <img src="<?= base_url('/images/banner_1.png') ?>" alt="Profile Picture">
+                    <img src="<?= base_url('/images/me.jpg') ?>" alt="Profile Picture">
                 </div>
                 <div class="info">
+                    <div>
                     <span class="icon">
                         <i class="fa-solid fa-lightbulb"></i>
                     </span>
                     <h3>Experience and Focus</h3>
+                    </div>
                     <p>I&apos;ve had the privilege of working on a wide range of projects—personal, contract-based, and full-time roles for companies. These experiences have given me hands-on exposure to solving real-world problems and building impactful solutions.
 
-                    While I&apos;ve gained proficiency in both frontend and backend development, my true passion lies in the backend. I like to describe myself as a &quot;backend guy who does frontend,&quot; rather than a full-stack developer. Backend development feels natural to me, and I enjoy the logic, problem-solving, and scalability it involves.</p>
+                        While I&apos;ve gained proficiency in both frontend and backend development, my true passion lies in the backend. I like to describe myself as a &quot;backend guy who does frontend,&quot; rather than a full-stack developer. Backend development feels natural to me, and I enjoy the logic, problem-solving, and scalability it involves.</p>
                 </div>
             </div>
         </div>
